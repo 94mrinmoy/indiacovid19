7d0697bc3e576d5940d7d017e2f4235725f74311
94mrinmoy <37420009+94mrinmoy@users.noreply.github.com>
2020-04-02 21:53:59 +0530

    full02

--- coronaIndia/urls.py
+++ coronaIndia/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index2, name='index2'),
     path('pic/', views.index, name='index'),
+    path('pic2/', views.index3, name='index3'),
 ]
--- coronaIndia/views.py
+++ coronaIndia/views.py
@@ -24,7 +24,7 @@ def index(request):
     #print (df)
     htmlPath = pm.plotMeInt()
     #plotlyDiv = s3.getPlotlyData(htmlPath)
-    s3.getPlotlyDataNew(htmlPath,settings.PROJECT_ROOT + '/coronaIndia/templates/example.html',settings.PROJECT_ROOT + '/coronaIndia/templates/indexMain.html')
+    s3.getPlotlyDataNew(htmlPath,settings.PROJECT_ROOT + '/coronaIndia/templates/abc.html',settings.PROJECT_ROOT + '/coronaIndia/templates/indexMain.html')
     context = {
         'df':data,
         'total':total,
@@ -33,7 +33,7 @@ def index(request):
         # 'plotlyDiv' : plotlyDiv
     }
     #print(plotlyDiv)
-    return render(request, 'index.html', context)
+    return render(request, 'indexMain.html', context)
     #return render(request, 'example.html', context)
     #return render (dfhtml.data)
 
@@ -83,5 +83,56 @@ def index2(request):
         prvCountFile.write(str(total))
         prvCountFile.close()
     return render(request, 'example.html', context)
+    # return render(request, 'indexdup.html', context)
+    #return render (dfhtml.data)
+
+
+    
+def index3(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    df,total,cTotal,icount,dcount,allTotal = s3.main()
+    #print(df[1][1])
+    #dfhtml = HTML(df.to_html(index=False))
+    df = df.sort_values(by=['Corona Affected Count'],ascending=False)
+    dfStates = list((df['States']))
+    dfAffected = list(df['Corona Affected Count'])
+    dfCured = list(df['Cured Count'])
+    dfDeath = list(df['Death Count'])
+    dfPop = list(df['Total Population'])
+    data = []
+    dictAffectedData = {}
+    dictCuredData = {}
+    dictDeathData={}
+    for i,s in enumerate(dfStates):
+        dictAffectedData.update({s:dfAffected[i]})
+        dictCuredData.update({s:dfCured[i]})
+        dictDeathData.update({s:dfDeath[i]})
+    #print(dictAffectedData)        
+    for i in range(len(dfStates)):
+        data.append([dfStates[i],dfAffected[i],dfCured[i],dfPop[i],dfDeath[i]])
+    #print (df)
+    pm.plotMeLatest()
+    # htmlPath = pm.plotMeInt()
+    # s3.getPlotlyDataNew(htmlPath,settings.PROJECT_ROOT + '/coronaIndia/templates/indexdup.html',settings.PROJECT_ROOT + '/coronaIndia/templates/indexMain.html')
+    context = {
+        'df':data,
+        'total':allTotal+cTotal,
+        'cTotal':cTotal,
+        'dcount':dcount,
+        'dictAffectedData':dictAffectedData,
+        'dictCuredData':dictCuredData,
+        'dictDeathData':dictDeathData
+    }
+    prvCountFile = open('aTotal.txt')
+    prvCount = prvCountFile.read()
+    prvCountFile.close()
+    if True:#(total>int(prvCount)):
+        #print('here')
+        gi.getImage(dictAffectedData,dictCuredData,icount,cTotal,dcount,allTotal)
+        pm.plotMeLatest()
+        prvCountFile = open('aTotal.txt','w+')
+        prvCountFile.write(str(total))
+        prvCountFile.close()
+    return render(request, 'indexMain.html', context)
     # return render(request, 'indexdup.html', context)
     #return render (dfhtml.data)
