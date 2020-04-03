805dec9194807e44e0187bca4e950fbb93daa1ec
94mrinmoy <37420009+94mrinmoy@users.noreply.github.com>
2020-04-03 20:13:11 +0530

    full0304

--- coronaIndia/urls.py
+++ coronaIndia/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path
-
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
     path('', views.index2, name='index2'),
     path('pic/', views.index, name='index'),
     path('pic2/', views.index3, name='index3'),
+    path('sw.js', (TemplateView.as_view(template_name="sw.js",content_type='application/javascript', )), name='sw.js'),
 ]
